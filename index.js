5a21314eaa89c161a7f03e4ea122144daaf12024
Abhishek <abhishekk10112001@gmail.com>
2024-07-24 14:08:26 +0530

    Update CORS configuration to allow Netlify app

--- index.js
+++ index.js
@@ -6,26 +6,54 @@ global.foodData = require('./db')(function call(err, data, CatData) {
   global.foodCategory = CatData;
 })
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(express.json())
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+// app.use(express.json())
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.use('/api/auth', require('./Routes/Auth'));
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on http://localhost:${port}`)
+// })
+
+
+
+// CORS configuration
+const corsOptions = {
+  origin: '*', // Allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.use('/api/auth', require('./Routes/Auth'));
 
+// Start server
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
 
